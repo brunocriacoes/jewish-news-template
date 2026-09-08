e7ab856ce7eb813948f1d6b600c6e0feaa44c07c
Bruno Vieira <br.rafael@outlook.com>
2026-09-08 15:32:06 -0300

    feat: add WordPress theme templates

---
+++ page.php
@@ -0,0 +1,1 @@
+<?php get_header(); ?><main id="conteudo" class="shell narrow section-block"><?php while(have_posts()):the_post(); ?><article class="entry-content"><h1><?php the_title(); ?></h1><?php the_content(); ?></article><?php endwhile; ?></main><?php get_footer(); ?>
