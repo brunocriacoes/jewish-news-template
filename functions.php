b573e572cb99745909e93e49e8cf085f1b7a60e0
Bruno Vieira <br.rafael@outlook.com>
2026-09-15 10:27:54 -0300

    feat: add editable footer menu locations

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-function jnt_setup(): void { add_theme_support('title-tag'); add_theme_support('post-thumbnails'); add_theme_support('html5', ['search-form','style','script']); register_nav_menus(['primary'=>__('Navegação principal','jewish-news-template'),'footer'=>__('Rodapé','jewish-news-template')]); }
+function jnt_setup(): void { add_theme_support('title-tag'); add_theme_support('post-thumbnails'); add_theme_support('html5', ['search-form','style','script']); register_nav_menus(['primary'=>__('Navegação principal','jewish-news-template'),'footer_sections'=>__('Rodapé: secções','jewish-news-template'),'footer_contact'=>__('Rodapé: contacto','jewish-news-template')]); }
 add_action('after_setup_theme','jnt_setup');
 function jnt_assets(): void { wp_enqueue_style('jnt-style',get_stylesheet_uri(),[], (string) filemtime(get_stylesheet_directory() . '/style.css')); wp_enqueue_script('jnt-theme',get_theme_file_uri('/assets/js/theme.js'),[], (string) filemtime(get_theme_file_path('/assets/js/theme.js')),true); }
 add_action('wp_enqueue_scripts','jnt_assets');
